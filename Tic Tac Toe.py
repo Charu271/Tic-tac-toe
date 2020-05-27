59c7dcf6264e9ab8ef8923c5ad1720511647a349
CHARU SACHDEVA <60181628+Charu271@users.noreply.github.com>
2020-05-27 22:42:56 +0530

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,14 +1,6 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 def clear():
     import os
     import time
-    # for windows
     os.system('clear')
     #os.system("ls")
     time.sleep(2)
